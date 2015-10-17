8fa44067e2897064bf1a23fc2d00ce4f43efbbaf
M0ritzWeide <Moritz@Moritz-MBP.lan>
2015-10-17 13:55:47 +0200

    Änderungen

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ $app->get('/get/:typ/:year/:month(/:day)', function($typ, $year, $month, $day = 
 		SELECT * FROM sensoren WHERE (typ = :typ) AND (timestamp = :date OR substr(timestamp, 1, 7) = :date)
 	");
 
-	$sth->bindParam(":typ", (int)$typ, PDO::PARAM_INT);
+	$sth->bindParam(":typ", $typ, PDO::PARAM_INT);
 	$sth->bindParam(":date", $date, PDO::PARAM_STR);
 	$sth->bindParam(":date", $date, PDO::PARAM_STR);
 
