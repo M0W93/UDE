7f63375b09002eee0624affcffaa4a6a53212ed4
M0ritzWeide <Moritz@Moritz-MBP.lan>
2015-10-17 13:29:28 +0200

    Änderung der DB Abfrage

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ $app->get('/get/:typ/:year/:month(/:day)', function($typ, $year, $month, $day = 
 
 
 	$sth = $db->prepare("
-		SELECT * FROM sensoren WHERE typ = :typ AND (timestamp LIKE :date OR substr(timestamp, 1, 7) LIKE :date)
+		SELECT * FROM sensoren WHERE (typ = :typ) AND (timestamp LIKE :date OR substr(timestamp, 1, 7) LIKE :date)
 	");
 
 	$sth->bindParam(":typ", $typ, PDO::PARAM_INT);
