a41fd82ebd08ac678da8a4335c0542a4d1a71609
M0ritzWeide <Moritz@Moritz-MBP.lan>
2015-10-18 09:42:12 +0200

    Breaks hinzugefügt

--- index.php
+++ index.php
@@ -74,7 +74,7 @@ $app->get('/parse', function () {
 		if (($handle = fopen("stationdata-17-10-2015.csv.json", "r")) !== FALSE) {
 		  while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
 		  	if(empty(getEuCode($data[0])[1])){
-
+		  		break;
 		  	}else{
 			    $sth = $db->prepare("
 			    	INSERT INTO sensoren 
